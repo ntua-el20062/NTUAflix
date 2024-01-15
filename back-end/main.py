ce9f0e2d92309edfcf177b510b71ef978261c190
ntua-el20131 <150613060+ntua-el20131@users.noreply.github.com>
2024-01-15 21:19:00 +0200

    update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -108,7 +108,7 @@ class reset_all(Resource):
 #######################################################################################
 
 def create_title_object(titleID):
-    title_query = "SELECT * FROM titles WHERE Title_id = %s"
+    title_query = "SELECT * FROM titlebasics WHERE tconst = %s"
     title_rows = execute_query(title_query, (titleID, ), fetch_data_flag=True, fetch_all_flag=True)
     
     if not title_rows:
@@ -116,71 +116,71 @@ def create_title_object(titleID):
 
     title_row = title_rows[0]
     titleObject = {
-        "titleID": title_row["Title_id"],
-        "type": title_row["Type"],
-        "originalTitle": title_row["Original_Title"],
-        "titlePoster": title_row["img_url"],
-        "startYear": str(title_row["Start_Year"]),
-        "endYear": str(title_row["End_Year"]) if title_row["End_Year"] else None,
+        "titleID": title_row["tconst"],
+        "type": title_row["titleType"],
+        "originalTitle": title_row["originalTitle"],
+        "titlePoster": title_row["img_url_asset"],
+        "startYear": str(title_row["startYear"]),
+        "endYear": str(title_row["endYear"]) if title_row["endYear"] else None,
         "genres": title_row["genres"].split(",") if title_row["genres"] else []
     }
 
-    aka_query = "SELECT Title, Region FROM aka WHERE Title_id = %s"
+    aka_query = "SELECT title, region FROM akas WHERE titleId = %s"
     title_akas = execute_query(aka_query, (titleID,), fetch_data_flag=True, fetch_all_flag=True)
-    titleObject["titleAkas"] = [{"akaTitle": row["Title"], "regionAbbrev": row["Region"]} for row in title_akas]
+    titleObject["titleAkas"] = [{"akaTitle": row["title"], "regionAbbrev": row["region"]} for row in title_akas]
     
     principals_query = """
-    SELECT pt.Professional_id, pt.Category, p.Primary_Name 
-    FROM professionals_titles pt
-    JOIN professionals p ON pt.Professional_id = p.Professional_id
-    WHERE pt.Title_id = %s
+    SELECT pt.nconst, pt.category, p.primaryName 
+    FROM principals pt
+    JOIN namebasics p ON pt.nconst = p.nconst
+    WHERE pt.tconst = %s
     """
     principals_data = execute_query(principals_query, (titleID,), fetch_data_flag=True, fetch_all_flag=True)
     principals = [
         {
-            "nameID": row["Professional_id"],
-            "name": row["Primary_Name"],
-            "category": row["Category"]
+            "nameID": row["nconst"],
+            "name": row["primaryName"],
+            "category": row["category"]
         } for row in principals_data
     ] if principals_data else []
 
     titleObject["principals"] = principals
 
-    rating_query = "SELECT Average_Rating, Number_of_Votes FROM ratings WHERE Title_id = %s"
+    rating_query = "SELECT averageRating, numVotes FROM ratings WHERE tconst = %s"
     rating_rows = execute_query(rating_query, (titleID,), fetch_data_flag=True, fetch_all_flag=True)
-    rating_row = rating_rows[0]
-    if rating_row:
+    #rating_row = rating_rows[0]
+    for rating_row in rating_rows:
         titleObject["rating"] = {
-            "avRating": str(rating_row["Average_Rating"]),
-            "nVotes": str(rating_row["Number_of_Votes"])
+            "avRating": str(rating_row["averageRating"]),
+            "nVotes": str(rating_row["numVotes"])
         }
     return titleObject
 
 def create_name_object(nameID):
-    contributor_query = "SELECT * FROM professionals WHERE Professional_id = %s"
+    contributor_query = "SELECT * FROM namebasics WHERE nconst = %s"
     contributor_rows = execute_query(contributor_query, (nameID,), fetch_data_flag=True, fetch_all_flag=True)
     contributor_row = contributor_rows[0]
     if not contributor_row:
         return None
 
     nameObject = {
-        "nameID": contributor_row["Professional_id"],
-        "name": contributor_row["Primary_Name"],
+        "nameID": contributor_row["nconst"],
+        "name": contributor_row["primaryName"],
         "namePoster": contributor_row["img_url_asset"],
-        "birthYr": str(contributor_row["Birth_Year"]) if contributor_row["Birth_Year"] else None,
-        "deathYr": str(contributor_row["Death_Year"]) if contributor_row["Death_Year"] else None,
-        "profession": contributor_row["Primary_Profession"],
+        "birthYr": str(contributor_row["birthYear"]) if contributor_row["birthYear"] else None,
+        "deathYr": str(contributor_row["deathYear"]) if contributor_row["deathYear"] else None,
+        "profession": contributor_row["primaryProfession"],
         "nameTitles": []
     }
 
     titles_query = """
-    SELECT pt.Title_id, pt.Category 
-    FROM professionals_titles pt
-    WHERE pt.Professional_id = %s
+    SELECT pt.tconst, pt.category 
+    FROM principals pt
+    WHERE pt.nconst = %s
     """
     titles_data = execute_query(titles_query, (nameID,), fetch_data_flag=True, fetch_all_flag=True)
     if titles_data:
-        nameObject["nameTitles"] = [{"titleID": row["Title_id"], "category": row["Category"]} for row in titles_data]
+        nameObject["nameTitles"] = [{"titleID": row["tconst"], "category": row["category"]} for row in titles_data]
 
     return nameObject
 
@@ -195,15 +195,15 @@ class get_title(Resource):
 
 def search_titles_by_title_part(titlePart):
     search_query = """
-    SELECT Title_id 
-    FROM titles 
-    WHERE Original_Title LIKE %s
+    SELECT tconst 
+    FROM titlebasics 
+    WHERE originalTitle LIKE %s
     """
     title_ids = execute_query(search_query, ('%' + titlePart + '%',), fetch_data_flag=True, fetch_all_flag=True)
     if not title_ids:
         return []
 
-    title_objects = [create_title_object(title_id['Title_id']) for title_id in title_ids]
+    title_objects = [create_title_object(title_id['tconst']) for title_id in title_ids]
     return title_objects
 
 class search_title(Resource):
@@ -219,12 +219,12 @@ class search_title(Resource):
 
 def search_titles_by_genre(qgenre, minrating, yrFrom=None, yrTo=None):
     genre_condition = "FIND_IN_SET(%s, genres)"
-    rating_condition = "Title_id IN (SELECT Title_id FROM ratings WHERE Average_Rating >= %s)"
-    year_condition = "(Start_Year >= %s AND Start_Year <= %s)" if yrFrom and yrTo else "1=1"
+    rating_condition = "tconst IN (SELECT tconst FROM ratings WHERE averageRating >= %s)"
+    year_condition = "(startYear >= %s AND startYear <= %s)" if yrFrom and yrTo else "1=1"
     
     search_query = f"""
-    SELECT Title_id
-    FROM titles
+    SELECT tconst
+    FROM titlebasics
     WHERE {genre_condition} AND {rating_condition} AND {year_condition}
     """
     values = [qgenre, minrating]
@@ -236,7 +236,7 @@ def search_titles_by_genre(qgenre, minrating, yrFrom=None, yrTo=None):
     if not title_ids:
         return []
 
-    title_objects = [create_title_object(title_id['Title_id']) for title_id in title_ids]
+    title_objects = [create_title_object(title_id['tconst']) for title_id in title_ids]
     return title_objects
 
 class by_genre(Resource):
@@ -262,16 +262,16 @@ class get_name(Resource):
 
 def search_names_by_name_part(namePart):
     search_query = """
-    SELECT Professional_id
-    FROM professionals
-    WHERE Primary_Name LIKE %s
+    SELECT nconst
+    FROM namebasics
+    WHERE primaryName LIKE %s
     """
     professional_ids = execute_query(search_query, ('%' + namePart + '%',), fetch_data_flag=True, fetch_all_flag=True)
 
     if not professional_ids:
         return []
 
-    name_objects = [create_name_object(professional_id['Professional_id']) for professional_id in professional_ids]
+    name_objects = [create_name_object(professional_id['nconst']) for professional_id in professional_ids]
     return name_objects
 
 class search_name(Resource):
@@ -285,6 +285,34 @@ class search_name(Resource):
         name_objects = search_names_by_name_part(name_part)
         return name_objects, 200 if name_objects else 404
 
+def get_top_10_titles_by_genre(genre):
+    print("hii")
+    search_query = """
+    SELECT t.tconst
+    FROM titlebasics t
+    JOIN ratings r ON t.tconst = r.tconst
+    WHERE FIND_IN_SET(%s, t.genres)
+    ORDER BY r.averageRating DESC
+    LIMIT 10
+    """
+    title_ids = execute_query(search_query, (genre,), fetch_data_flag=True, fetch_all_flag=True)
+
+    if not title_ids:
+        return []
+
+    title_objects = [create_title_object(title_id['tconst']) for title_id in title_ids]
+    return title_objects
+
+class top10_by_genre(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('genre', type=str, required=True, location='json')
+
+    def get(self):
+        args = self.reqparse.parse_args()
+        genre = args['genre']
+        top_titles = get_top_10_titles_by_genre(genre)
+        return top_titles, 200 if top_titles else 404
 
 api = Api(app, prefix='/ntuaflix_api')
 
@@ -305,6 +333,7 @@ api.add_resource(search_title, '/searchtitle')
 api.add_resource(by_genre, '/bygenre')
 api.add_resource(get_name, '/name/<string:nameID>')
 api.add_resource(search_name, '/searchname')
+api.add_resource(top10_by_genre, '/top10bygenre')
 
 if __name__ == "__main__":  
     app.run(debug=True, port='9876')
