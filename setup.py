d33f4580ee63b25649934ff3800022add7856044
ntua-el20035 <el20035@mail.ntua.gr>
2024-01-22 16:40:19 +0200

    added installing dependencies script

--- setup.py
+++ setup.py
@@ -1,1 +1,5 @@
-# needs to install packages, setup database, run the API server, run the webserver
+# , setup database, run the API server, run the webserver
+from util.dependencies import install_dependencies
+
+# Install all dependencies
+install_dependencies()
