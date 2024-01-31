85e615be9539f115fb45d63fa100493f579ed5aa
iris faidon <el20035@mail.ntua.gr>
2024-01-31 15:14:24 +0200

    praying it works this time

--- front-end/server.py
+++ front-end/server.py
@@ -17,35 +17,11 @@ DUMMY_ACTOR = {'name': 'Dummy Actor', 'bio': 'This is a dummy bio', 'img_url_ass
 DUMMY_MOVIE_DETAILS = {'title': 'Dummy Movie', 'rating': {'avRating': '8.1'}, 'genres': ['Action', 'Drama']}
 DUMMY_SERIES_DETAILS = {'title': 'Dummy Series', 'summary': 'This is a dummy series'}
 
-DEFAULT_MOVIE_IMAGE_URL = "https://icons.iconarchive.com/icons/designbolts/free-multimedia/512/Film-icon.png"
-DEFAULT_PERSON_IMAGE_URL = "https://cdn-icons-png.flaticon.com/512/3060/3060028.png"
-
-def process_image_urls(data, width='w500'):
-    if isinstance(data, list):
-        for item in data:
-            process_image_urls(item, width)
-    elif isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(value, (dict, list)):
-                process_image_urls(value, width)
-            elif 'namePoster' in key:
-                if value and '{width_variable}' in value:
-                    data[key] = value.replace('{width_variable}', width)
-                else:
-                    data[key] = DEFAULT_PERSON_IMAGE_URL
-            elif 'titlePoster' in key:
-                if value and '{width_variable}' in value:
-                    data[key] = value.replace('{width_variable}', width)
-                else:
-                    data[key] = DEFAULT_MOVIE_IMAGE_URL
-
-
 @app.route('/')
 def homepage():
     try:
         response = requests.get(f"{API_BASE_URL}/top10bygenre")
         genres_data = response.json() if response.status_code == 200 else {}
-        process_image_urls(genres_data)
     except requests.RequestException:
         genres_data = DUMMY_GENRES_DATA
     return render_template('homepage.html', genres_data=genres_data)
@@ -54,30 +30,24 @@ def homepage():
 def searchpage():
     query = request.args.get('query', '')
     try:
-        # Fetch movies
-        response_movies = requests.get(f"{API_BASE_URL}/searchtitle", json={'titlePart': query})
-        movies = response_movies.json() if response_movies.status_code == 200 else []
-        process_image_urls(movies)
-
-        # Fetch people
-        response_people = requests.get(f"{API_BASE_URL}/searchname", json={'namePart': query})
-        people = response_people.json() if response_people.status_code == 200 else []
-        print(people)
-        process_image_urls(people)
-
+        response = requests.get(f"{API_BASE_URL}/searchtitle", json={'titlePart': query})
+        movies = response.json() if response.status_code == 200 else []
     except requests.RequestException:
         movies = DUMMY_MOVIES
-        people = []  # Add dummy data for people if needed
+    return render_template('searchpage.html', movies=movies)
 
-    return render_template('searchpage.html', movies=movies, actors=people)
-
-
-#something needs to change here in order to get the movie title 
 @app.route('/actor/<actor_name>')
 def actor_page(actor_name):
     try:
         response = requests.get(f"{API_BASE_URL}/name/{actor_name}")
         actor_details = response.json() if response.status_code == 200 else {}
+        movies = []
+        for title in actor_details["nameTitles"]:
+            titleID = title["titleID"]
+            title_response = requests.get(f"{API_BASE_URL}/title/{titleID}")
+            if title_response.status_code == 200:
+                movies.append(title_response.json())
+        actor_details["movies"] = movies
     except requests.RequestException:
         actor_details = DUMMY_ACTOR
     return render_template('actor_page.html', actor=actor_details)
@@ -104,7 +74,6 @@ def series_page(series_title):
     try:
         response = requests.get(f"{API_BASE_URL}/title/{series_title}")
         series_details = response.json() if response.status_code == 200 else {}
-        process_image_urls(series_details)
     except requests.RequestException:
         series_details = DUMMY_SERIES_DETAILS
     return render_template('series_page.html', series=series_details)
@@ -114,11 +83,72 @@ def genre_page(genre_name):
     try:
         response = requests.get(f"{API_BASE_URL}/bygenre", json={'qgenre': genre_name, 'minrating': '0'})
         movies = response.json() if response.status_code == 200 else []
-        process_image_urls(movies)
     except requests.RequestException:
+        # Fallback to dummy data for the genre
         movies = DUMMY_GENRES_DATA.get(genre_name, [])
     return render_template('genre_page.html', genre_name=genre_name, movies=movies)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+# from flask import Flask, render_template
+# import os
+
+# app = Flask(__name__)
+
+# # Set the templates folder to the "templates" subdirectory
+# app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+
+# # Define sample data (replace this with your actual data)
+# genres = ['Action', 'Drama', 'Comedy']
+# movies = [
+#     {'title': 'Movie 1', 'rating': 8.5, 'genre': 'Action'},
+#     {'title': 'Movie 2', 'rating': 7.9, 'genre': 'Drama'},
+#     {'title': 'Movie 3', 'rating': 6.5, 'genre': 'Comedy'},
+# ]
+# actors = [
+#     {'name': 'Actor 1', 'bio': 'Bio for Actor 1'},
+#     {'name': 'Actor 2', 'bio': 'Bio for Actor 2'},
+# ]
+
+# # Define routes
+# @app.route('/')
+# def homepage():
+#     return render_template('homepage.html', genres=genres, movies=movies)
+
+# @app.route('/search')
+# def searchpage():
+#     return render_template('searchpage.html', movies=movies, actors=actors)
+
+# @app.route('/actor/<actor_name>')
+# def actor_page(actor_name):
+#     # Fetch actor details from the database based on actor_name
+#     actor_details = {'name': actor_name, 'bio': 'Bio for ' + actor_name}
+#     return render_template('actor_page.html', actor=actor_details)
+
+# @app.route('/movie/<movie_title>')
+# def movie_page(movie_title):
+#     # Fetch movie details from the database based on movie_title
+#     movie_details = {'title': movie_title, 'rating': 8.5, 'genre': 'Action'}
+#     return render_template('movie_page.html', movie=movie_details)
+
+# @app.route('/series/<series_title>')
+# def series_page(series_title):
+#     # Fetch series details from the database based on series_title
+#     series_details = {'title': series_title, 'seasons': 5, 'episodes': 10}
+#     return render_template('series_page.html', series=series_details)
+
+# @app.route('/genre/<genre_name>')
+# def genre_page(genre_name):
+#     # Fetch movies in the specified genre from the database
+#     genre_movies = [movie for movie in movies if movie['genre'] == genre_name]
+#     return render_template('genre_page.html', genre_name=genre_name, movies=genre_movies)
+
+
+# app.run(debug=True)
 
