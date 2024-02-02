3ebf5fe6a949884665f079067bc7d99d02ac9166
ntua-el20131 <150613060+ntua-el20131@users.noreply.github.com>
2024-02-02 13:56:29 +0200

    update main.py

--- back_end/API/main.py
+++ back_end/API/main.py
@@ -26,6 +26,7 @@ api.add_resource(by_genre, '/bygenre')
 api.add_resource(get_name, '/name/<string:nameID>')
 api.add_resource(search_name, '/searchname')
 api.add_resource(top10_by_genre, '/top10bygenre')
+api.add_resource(all_by_genre, '/allbygenre')
 
 if __name__ == "__main__":  
     app.run(debug=True, port=9876)
